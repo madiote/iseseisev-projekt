3c059ae30449613c1612e49d7f1108fdfbabb482
Madis Otenurm <43170891+madiote@users.noreply.github.com>
2019-04-22 12:07:25 +0300

    PWA and SW

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /* jshint esversion:6 */
 
-
 function forceHttps() {
     // Ensures that the Greeny page is loaded over HTTPS - https://stackoverflow.com/a/4597085
     if (window.location.href.indexOf("greeny.cs.tlu.ee") != -1) {
@@ -13,3 +12,21 @@ function forceHttps() {
 window.onload = function () {
     forceHttps();
 };
+
+window.addEventListener("online", function () {
+    console.log("Sa oled netis!");
+});
+
+window.addEventListener("offline", function () {
+    console.log("Nett on otsas!");
+});
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js', {
+        scope: '/'
+    }).then(function (sw) {
+        console.log("SW olemas!");
+    }).catch(function () {
+        console.log("SW ebaõnnestus!");
+    });
+}
