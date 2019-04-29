9c4e65ef0a567548bbb0d2c56fc8444d047d0694
Madis Otenurm <43170891+madiote@users.noreply.github.com>
2019-04-29 10:41:09 +0300

    sliding_puzzle

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const columns = 10;
 const blocks = [1, 2, 3, 4];
 const colours = ["#FF9AA2", "#FFB7B2",  "#FFDAC1", "#E2F0CB", "#B5EAD7", "#C7CEEA"];
 
+let currentScore;
+let bestScore;
+
+function appendBottomRow(){
+
+}
+
+function death(){
+    if (currentScore > bestScore){
+        bestScore = currentScore;
+    }
+}
+
+function restart(){
+    currentScore = 0;
+    location.reload();
+}
+
+
 function forceHttps() {
     // Ensures that the Greeny page is loaded over HTTPS - https://stackoverflow.com/a/4597085
     if (window.location.href.indexOf("greeny.cs.tlu.ee") != -1) {
