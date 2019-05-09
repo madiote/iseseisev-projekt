c9067f96b7201e001c4b7efd35428778433fb705
Madis <Madis0@users.noreply.github.com>
2019-05-09 20:00:46 +0300

    SW fix

--- sw.js
+++ sw.js
@@ -37,24 +37,3 @@ self.addEventListener('fetch', function (event) {
     })
   );
 });
-
-
-self.addEventListener('fetch', function (event) {
-  console.log(event.request);
-
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || event.default();
-    })
-  );
-});
-
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open('static-v1').then(function (cache) {
-      return cache.addAll([
-        '/~madiote/eesrakendused/iseseisev-projekt/'
-      ]);
-    })
-  );
-});
